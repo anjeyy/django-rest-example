e3024fd46d502b8c4a93a99df4bbd816d6fc2928
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2022-03-27 14:42:52 +0200

    basic rest operations

--- rest/rest_api/models.py
+++ rest/rest_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Document(models.Model):
+    title = models.CharField(max_length=256)
+    person = models.CharField(max_length=256)
+    file_size = models.PositiveIntegerField()
